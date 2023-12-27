e18eb34452ce28ab843fdbacc86254e2b93567ed
Kukvly <kukvly96@gmail.com>
2023-12-27 21:20:44 +0900

    First Swift Commit

---
+++ pracSwift.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+print(greeting)
+
+var x = 10
+for index in 1...20{
+    let y = index * x
+    x -= 1
+    print(y)
+}
+print("Welcome to Swift")
+/*:
+# Welcome to pracSwift
+This is your *first* playground which is intended to demonstrate:
+* The use of **Quick Look**
+* Placing results **in-line** with the code
+*/
